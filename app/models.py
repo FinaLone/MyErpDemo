63f124185ea84b5624ee932abfb98460ff9b371d
sagestar <lv_sage@126.com>
2016-10-18 23:11:38 +0800

    登录功能制作中1
    1

--- app/models.py
+++ app/models.py
@@ -4,13 +4,15 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-from . import db
+from . import db, login_manager
+from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 #   用户角色表roles
 class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'TRoleInfo'
+    role_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
@@ -19,18 +21,32 @@ class Role(db.Model):
 
 
 #   用户表TUserInfo
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'TUserInfo'
-    id = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    user_id = db.Column(db.Integer, primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('TRoleInfo.role_id'))
     user_name = db.Column(db.String(64), unique=True, index=True)
     login_time = db.Column(db.DateTime)
     register_time = db.Column(db.DateTime)
-    pwd = db.Column(db.String(32))
+    pwd_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('密码是不可读取的！')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user_name
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #   注册码表TRegisterCode
 class RegisterCode(db.Model):
